e49c85e4c5a60e341dc02413117b311b8bae39ef
imielin <maciekbojdak@gmail.com>
2015-03-09 21:27:04 +0100

    Add Test.java

--- Test.java
+++ Test.java
@@ -11,6 +11,22 @@ public class Test {
 		
 		System.out.println("Ada jesli serio to bedziesz czytaæ to to siê kurna równa " +fibbonachi(n)+" ");
 		// TODO Auto-generated method stub
+		System.out.println("chcesz sie w to dalej bawiæ? (t/n) ");
+		String text = new Scanner(System.in).nextLine();
+		char txt= text.charAt(0);
+		if(txt=='t'){
+			System.out.println("Ada serio? to trudno ");	
+		}
+		else if(txt=='n'){
+			System.exit(0);
+		}
+		else {
+			System.out.println("taki z ciebie cwaniak? Ÿle wpisane i teraz bêdzie œmieræ");
+			while(true){
+				System.out.println("ŒMIERÆ ");	
+				java.awt.Toolkit.getDefaultToolkit().beep();
+			}
+		}
 		}
 
 	}
