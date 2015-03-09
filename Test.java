597b273b92615408a648363b7c464d661d086555
imielin <maciekbojdak@gmail.com>
2015-03-09 20:44:17 +0100

    Add Test.java

---
+++ Test.java
@@ -0,0 +1,25 @@
+package test;
+
+import java.util.Scanner;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		while(true){
+		System.out.println("podaj liczbe");
+		int n= new Scanner(System.in).nextInt();
+		
+		System.out.println("Ada jesli serio to bedziesz czytaæ to to siê kurna równa " +fibbonachi(n)+" ");
+		// TODO Auto-generated method stub
+		}
+
+	}
+	public static int fibbonachi(int n)
+	{
+		if(n==1 || n==2){
+			return 1;
+		}
+		return fibbonachi(n-1) + fibbonachi(n-2);
+	}
+
+}
